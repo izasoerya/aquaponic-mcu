f1e9526d68894e3bee94b19a7de58dbbf4903169
izasoerya <ihza.surya@gmail.com>
2026-07-22 11:23:10 +0700

    chore: explicit reset reason esp

--- utils/parser.h
+++ utils/parser.h
@@ -22,6 +22,37 @@ public:
             return AppState::NORMAL_MODE;
         }
     }
+
+    static String parseResetReasonESP(esp_reset_reason_t reason)
+    {
+        switch (reason)
+        {
+        case ESP_RST_UNKNOWN:
+            return "Unknown";
+        case ESP_RST_POWERON:
+            return "Power On Reset";
+        case ESP_RST_EXT:
+            return "External Pin Reset";
+        case ESP_RST_SW:
+            return "Software Reset";
+        case ESP_RST_PANIC:
+            return "Hardware Panic / Exception Reset";
+        case ESP_RST_INT_WDT:
+            return "Interrupt Watchdog Reset";
+        case ESP_RST_TASK_WDT:
+            return "Task Watchdog Reset";
+        case ESP_RST_WDT:
+            return "Other Watchdog Reset";
+        case ESP_RST_DEEPSLEEP:
+            return "Deep Sleep Wakeup Reset";
+        case ESP_RST_BROWNOUT:
+            return "Brownout Reset (Voltage Dip)";
+        case ESP_RST_SDIO:
+            return "SDIO Reset";
+        default:
+            return "Undefined Code";
+        }
+    }
 };
 
 #endif // PARSER_H
